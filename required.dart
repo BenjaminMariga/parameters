119ab55c1f20fd3af72f4e8c407b51f161a58eeb
banjo254 <benjaminmariga@gmail.com>
2023-02-07 16:16:16 +0300

    required parameter

---
+++ required.dart
@@ -0,0 +1,10 @@
+//Required parameters in dart, you cant skip a paramater
+void main() {
+  cityName("Mumbai", "Cairo", "Nairobi");
+}
+
+void cityName(String City1, String City2, String City3) {
+  print("CIty name is:$City1");
+  print("CIty name is:$City2");
+  print("CIty name is:$City3");
+}
