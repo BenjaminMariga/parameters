eba0a29816233495035c81d36faf50bf32b3eb71
banjo254 <benjaminmariga@gmail.com>
2023-02-10 16:48:17 +0300

    making parameters optional

--- optional.dart
+++ optional.dart
@@ -1,13 +1,26 @@
 //optional parameters..
 //parameters can be skipped from the right sidde using []
+// ignore_for_file: dead_code
+
 void main() {
   countryName(
+    "Nairobi",
+    "Kenya",
+  );
+  print("");
+  countryNames(
     "Nairobi",
     "Kenya",
   );
 }
 
 void countryName(String Name1, String Name2, [String? Name3]) {
+  print(Name1);
+  print(Name2);
+  print(Name3);
+}
+
+void countryNames(String Name1, String Name2, {String? Name3}) {
   print(Name1);
   print(Name2);
   print(Name3);
