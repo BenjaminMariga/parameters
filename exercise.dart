f99bc15f5d725443bb617b5f98040e3589ae3b50
banjo254 <benjaminmariga@gmail.com>
2023-02-11 19:54:47 +0300

    simple dart exercise

---
+++ exercise.dart
@@ -0,0 +1,9 @@
+void main() {
+  youAreWonderful(
+    "Banjo",
+  );
+}
+
+String youAreWonderful(String name, {int numberPeople = 30}) {
+  return "You're wonderful $name $numberPeople people think so";
+}
